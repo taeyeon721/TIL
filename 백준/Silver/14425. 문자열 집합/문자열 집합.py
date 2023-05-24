74bc051bbf34deee1d0fe0e6279f5f42e6603bf2
taeyeon721 <109333511+taeyeon721@users.noreply.github.com>
2023-05-24 09:23:37 +0900

    [Silver III] Title: 문자열 집합, Time: 3696 ms, Memory: 36124 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+S = []
+cnt = 0
+for _ in range(N):
+    S.append(input())
+for _ in range(M):
+    if input() in S:
+        cnt += 1
+print(cnt)
