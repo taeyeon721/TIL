edb793e94485c5d0d7ea40079479ecff1cb57627
taeyeon721 <109333511+taeyeon721@users.noreply.github.com>
2023-06-12 20:44:13 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -2,18 +2,18 @@ import sys
 input = sys.stdin.readline
 
 T = int(input())
+dp = [1]*10001
+k = 1
+cnt = 1
+for i in range(1, 10001):
+    if cnt == 6:
+        cnt = 0
+        k += 1
+    if i % 6 == 1:
+        dp[i] = dp[i - 1] + k - 1
+    else:
+        dp[i] = dp[i-1] + k
+    cnt += 1
 for _ in range(T):
     n = int(input())
-    dp = [1]*(n+1)
-    k = 1
-    cnt = 1
-    for i in range(1, n+1):
-        if cnt == 6:
-            cnt = 0
-            k += 1
-        if i % 6 == 1:
-            dp[i] = dp[i - 1] + k - 1
-        else:
-            dp[i] = dp[i-1] + k
-        cnt += 1
     print(dp[n])
