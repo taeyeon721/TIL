89dd849797219965ffb5adea7ceba1dd7ae41040
taeyeon721 <109333511+taeyeon721@users.noreply.github.com>
2023-08-08 09:46:18 +0900

    [Silver I] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,14 +1,2 @@
-def power(X, Y):
-    if X == 1:
-        return Y
-    else:
-        tmp = power(X//2, Y)
-        if X % 2 == 1:
-            return tmp*tmp*Y%C
-        else:
-            return tmp*tmp%C
-
-
 A, B, C = map(int, input().split())
-ans = power(B, A)
-print(ans%C)
+print(pow(A, B, C))
