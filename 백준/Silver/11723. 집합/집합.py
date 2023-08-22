b0ef22f4237b71f90cff4f556c430788a57d4472
taeyeon721 <109333511+taeyeon721@users.noreply.github.com>
2023-08-22 20:13:27 +0900

    [Silver V] Title: 집합, Time: 3832 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,22 +1,24 @@
 import sys
 input = sys.stdin.readline
 M = int(input())
-S = set()
+S = 0
 for _ in range(M):
-    lst = list(input().split())
-    if lst[0] == 'add':
-        S.add(int(lst[1]))
-    elif lst[0] == 'remove':
-        if int(lst[1]) in S:
-            S.remove(int(lst[1]))
-    elif lst[0] == 'check':
-        print(1 if int(lst[1]) in S else 0)
-    elif lst[0] == 'toggle':
-        if int(lst[1]) in S:
-            S.remove(int(lst[1]))
-        else:
-            S.add(int(lst[1]))
-    elif lst[0] == 'all':
-        S = set(range(1, 21))
+    lst = input().split()
+    if lst[0] == 'all':
+        S = (1 << 20)-1
+    elif lst[0] == 'empty':
+        S = 0
     else:
-        S = set()
+        st = lst[0]
+        n = int(lst[1])-1
+        if st == 'add':
+            S |= (1 << n)
+        elif st == 'remove':
+            S &= ~(1 << n)
+        elif st == 'check':
+            if S & (1 << n) == 0:
+                print(0)
+            else:
+                print(1)
+        elif st == 'toggle':
+            S = S ^ (1 << n)
