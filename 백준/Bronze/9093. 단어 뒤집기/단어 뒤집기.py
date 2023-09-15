6184cf3c2460c47f8663fcfd2ee85e88258d628a
taeyeon721 <109333511+taeyeon721@users.noreply.github.com>
2023-09-15 23:54:46 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 140 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -4,7 +4,5 @@ T = int(input())
 for _ in range(T):
     lst = list(input().rstrip().split())
     for w in lst:
-        for i in range(1, len(w)+1):
-            print(w[-i], end='')
-        print(end=' ')
+        print(w[::-1], end=" ")
     print()
